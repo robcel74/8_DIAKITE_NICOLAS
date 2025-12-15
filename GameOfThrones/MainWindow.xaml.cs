d6fd371fbacdca8de7d04f96c38785f3f8bbe8e0
nicolasr <Robin.Nicolas@etu.univ-smb.fr>
2025-12-15 16:05:13 +0100

    Nouveaux sons/musiques

--- GameOfThrones/MainWindow.xaml.cs
+++ GameOfThrones/MainWindow.xaml.cs
@@ -1,13 +1,5 @@
-﻿using System.Text;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
+﻿using System.Windows;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace GameOfThrones
 {
@@ -24,8 +16,11 @@ namespace GameOfThrones
         {
             InitializeComponent();
             AfficheUCDemarrage();
-           
-        }      
+
+        }
+
+
+
         private void AfficheUCDemarrage()
         {
             UCDemarrage uc = new UCDemarrage();
@@ -52,14 +47,14 @@ namespace GameOfThrones
         private void AfficheUCJeu(object sender, RoutedEventArgs e)
         {
             UCJeu uc = new UCJeu();
-            AireJeu.Content=uc;
-            
+            AireJeu.Content = uc;
+
         }
 
         private void AfficheUCParametres(object sender, RoutedEventArgs e)
         {
             UCParametres uc = new UCParametres();
-            AireJeu.Content=uc;
+            AireJeu.Content = uc;
             uc.but_retour.Click += RetourUCDemarrage;
         }
 
@@ -83,5 +78,7 @@ namespace GameOfThrones
             AireJeu.Content = uc;
 
         }
+
+
     }
 }
